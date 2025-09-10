ba0754f933e23c51bc9e8ea41774e75379a0ce1d
Prakhar Chauhan <157095910+Prakharchauhan55@users.noreply.github.com>
2025-09-10 18:22:40 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 9/10/2025, 6:22:40 PM
+class Solution {
+  public boolean isPalindrome(int x) {
+    if (x < 0)
+      return false;
+
+    long reversed = 0;
+    int y = x;
+
+    while (y > 0) {
+      reversed = reversed * 10 + y % 10;
+      y /= 10;
+    }
+
+    return reversed == x;
+  }
+}
