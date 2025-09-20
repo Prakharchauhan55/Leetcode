3da4ee3004e97f9a920b59fb75537499ea93c8bf
Prakhar Chauhan <157095910+Prakharchauhan55@users.noreply.github.com>
2025-09-20 23:39:01 +0530

    Create 2974-MinimumNumberGame

---
+++ 2974-MinimumNumberGame/2974-MinimumNumberGame.java
@@ -0,0 +1,12 @@
+// Last updated: 9/20/2025, 11:39:02 PM
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i+=2){
+            int temp=nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=temp;
+        }
+        return nums;
+    }
+}
