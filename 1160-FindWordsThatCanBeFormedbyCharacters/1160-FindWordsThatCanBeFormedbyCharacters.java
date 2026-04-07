62d8b0c9532a47e8727270b1b0f335f382784ba4
Prakhar Chauhan <157095910+Prakharchauhan55@users.noreply.github.com>
2026-04-07 11:30:39 +0530

    File updated at 4/7/2026, 11:30:40 AM

--- 1160-FindWordsThatCanBeFormedbyCharacters/1160-FindWordsThatCanBeFormedbyCharacters.java
+++ 1160-FindWordsThatCanBeFormedbyCharacters/1160-FindWordsThatCanBeFormedbyCharacters.java
@@ -1,13 +1,24 @@
-// Last updated: 4/7/2026, 7:25:34 AM
+// Last updated: 4/7/2026, 11:30:40 AM
 1class Solution {
-2    public boolean containsDuplicate(int[] nums) {
-3        HashSet<Integer> set = new HashSet<>();
-4        for (int i = 0; i < nums.length; i++) {
-5                if (set.contains(nums[i])) {
-6                    return true;
-7                }
-8                set.add(nums[i]);
-9        }
-10        return false;
-11    }
-12}
+2    public int[] intersection(int[] nums1, int[] nums2) {
+3        HashSet<Integer> set1 = new HashSet<>();
+4        for(int n : nums1){
+5            set1.add(n);
+6        }
+7
+8        HashSet<Integer> result = new HashSet<>();
+9        for(int n : nums2){
+10            if(set1.contains(n)){
+11                result.add(n);
+12            }
+13        }
+14
+15        int[] ans = new int[result.size()];
+16        int i = 0;
+17        for(int val : result){
+18            ans[i++] = val;
+19        }
+20
+21        return ans;
+22    }
+23}
