6cdb53623226124cd011a8c3e5bf38e9f95ed52e
Prakhar Chauhan <157095910+Prakharchauhan55@users.noreply.github.com>
2026-06-16 23:57:36 +0530

    Create

---
+++ problem-1781634455037/problem-1781634455037.java
@@ -0,0 +1,19 @@
+// Last updated: 6/16/2026, 11:57:35 PM
+1class Solution {
+2    public void rotate(int[] nums, int k) {
+3        int n = nums.length;
+4        k%=n;
+5        rotateArray(nums, 0, n-1);
+6        rotateArray(nums, 0, k-1);
+7        rotateArray(nums, k, n-1);
+8    }
+9    public static void rotateArray(int[] nums, int start, int end) {
+10        while (start < end) {
+11            int temp = nums[start];
+12            nums[start] = nums[end];
+13            nums[end] = temp;
+14            start++;
+15            end--;
+16        }
+17    }
+18}
