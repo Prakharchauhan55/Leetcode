3b65c15601d4025b1e5b7ac4e03569d03741cbbd
Prakhar Chauhan <157095910+Prakharchauhan55@users.noreply.github.com>
2026-06-16 19:11:49 +0530

    File updated at 6/16/2026, 7:11:48 PM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.java
@@ -1,19 +1,7 @@
-// Last updated: 4/3/2026, 11:16:10 AM
-1class Solution {
+// Last updated: 6/16/2026, 7:11:48 PM
+1public class Solution {
 2    public int findKthLargest(int[] nums, int k) {
-3        PriorityQueue<Integer> pq = new PriorityQueue<>();
-4        for(int num : nums){
-5            pq.add(num);
-6        }
-7        int n = nums.length-1;
-8        int x = n-k;
-9        
-10        for(int i = 0; i <= x; i++){
-11            pq.poll();
-12        }
-13
-14        return pq.peek();
-15        
-16
-17    }
-18}
+3        Arrays.sort(nums);
+4        return nums[nums.length - k];
+5    }
+6}
