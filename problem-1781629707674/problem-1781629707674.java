29b473d3e195de53d53311d3c5b1e1f584354da9
Prakhar Chauhan <157095910+Prakharchauhan55@users.noreply.github.com>
2026-06-16 22:38:28 +0530

    Create

---
+++ problem-1781629707674/problem-1781629707674.java
@@ -0,0 +1,16 @@
+// Last updated: 6/16/2026, 10:38:27 PM
+1class Solution {
+2    public boolean check(int[] nums) {
+3        int count = 0;
+4        int n = nums.length;
+5        for(int i = 0; i < nums.length; i++){
+6            if(nums[i] > nums[(i+1)%n]){
+7                count++;
+8            }
+9        }
+10        if(count > 1){
+11            return false;
+12        }
+13        return true;
+14    }
+15}
