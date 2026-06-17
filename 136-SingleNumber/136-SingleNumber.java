8ea22a1bd3b8c1329c0e18143579807a31f70a87
Prakhar Chauhan <157095910+Prakharchauhan55@users.noreply.github.com>
2026-06-17 08:52:22 +0530

    File updated at 6/17/2026, 8:52:22 AM

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,10 +1,10 @@
-// Last updated: 9/10/2025, 6:22:12 PM
-class Solution {
-  public int singleNumber(int[] nums) {
-    int res = 0;
-    for(int i = 0; i < nums.length; i++){
-        res = res ^ nums[i];
-    }
-    return res;
-  }
-}
+// Last updated: 6/17/2026, 8:52:22 AM
+1class Solution {
+2    public int singleNumber(int[] nums) {
+3        int XOR1 = 0;
+4        for(int i = 0; i < nums.length; i++){
+5            XOR1 = XOR1^nums[i];
+6        }
+7        return XOR1;
+8    }
+9}
