03f640033a0de8e7df348e085436e12f113e3227
Prakhar Chauhan <157095910+Prakharchauhan55@users.noreply.github.com>
2026-06-18 18:41:14 +0530

    File updated at 6/18/2026, 6:41:15 PM

--- 53-MaximumSubarray/53-MaximumSubarray.java
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -1,22 +1,16 @@
-// Last updated: 10/13/2025, 10:04:49 PM
-class Solution {
-    public int maxSubArray(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int sum = 0;
-        for(int i = 0; i < nums.length; i++){
-            sum += nums[i];
-            if(sum < 0){
-                sum = 0;
-            }
-            max = Math.max(max, sum);
-        }
-        if(max == 0){
-            int maxi = Integer.MIN_VALUE;
-            for(int i = 0; i < nums.length; i++){
-                maxi = Math.max(maxi, nums[i]);
-            }
-            max = maxi;
-        }
-        return max;
-    }
-}
+// Last updated: 6/18/2026, 6:41:15 PM
+1class Solution {
+2    public int maxSubArray(int[] nums) {
+3        int res = Integer.MIN_VALUE;
+4        int sum = 0;
+5
+6        for(int i = 0; i < nums.length; i++){
+7            sum += nums[i];
+8            res = Math.max(res, sum);
+9            if(sum < 0){
+10                sum = 0;
+11            }
+12        }
+13        return res;
+14    }
+15}
