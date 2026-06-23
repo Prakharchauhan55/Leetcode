4d788d8f94147e422de6488ae9fdf8afbb2d07d1
Prakhar Chauhan <157095910+Prakharchauhan55@users.noreply.github.com>
2026-06-23 15:01:19 +0530

    File updated at 6/23/2026, 3:01:20 PM

--- 1021-RemoveOutermostParentheses/1021-RemoveOutermostParentheses.java
+++ 1021-RemoveOutermostParentheses/1021-RemoveOutermostParentheses.java
@@ -1,21 +1,20 @@
-// Last updated: 1/17/2026, 11:56:36 AM
+// Last updated: 6/23/2026, 3:01:20 PM
 1class Solution {
 2    public String removeOuterParentheses(String s) {
-3        Stack<Character> bracket = new Stack<>();
-4        StringBuilder sb = new StringBuilder("");
-5        for(int i=0;i<s.length();i++){
-6            if(s.charAt(i)=='('){
-7                if(bracket.size()>0){
-8                    sb.append(s.charAt(i));
-9                }
-10                bracket.push(s.charAt(i));
-11            }else{
-12                bracket.pop();
-13                if(bracket.size()>0){
-14                    sb.append(s.charAt(i));
-15                }
-16            }
-17        }
-18        return sb.toString();
-19    }
-20}
+3        StringBuilder ans = new StringBuilder();
+4        int depth = 0;
+5
+6        for (char ch : s.toCharArray()) {
+7            if (ch == '(') {
+8                if (depth > 0) ans.append(ch);
+9                depth++;
+10            } 
+11            else {
+12                depth--;
+13                if (depth > 0) ans.append(ch);
+14            }
+15        }
+16
+17        return ans.toString();
+18    }
+19}
