620136494baa7fcfaec80d3ab083a158d71c465f
Prakhar Chauhan <157095910+Prakharchauhan55@users.noreply.github.com>
2026-06-24 13:54:13 +0530

    Create

---
+++ problem-1782289453122/problem-1782289453122.java
@@ -0,0 +1,15 @@
+// Last updated: 6/24/2026, 1:54:13 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) { val = x; }
+7 * }
+8 */
+9class Solution {
+10    public void deleteNode(ListNode node) {
+11        node.val = node.next.val;
+12        node.next = node.next.next;
+13    }
+14}
